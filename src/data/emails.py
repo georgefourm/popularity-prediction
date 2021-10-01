2e19aec7e9aeb8f098818106fa523627cda1d26b
George Fourmouzis <georgefourm@gmail.com>
2021-10-01 13:27:46 +0300

    Fix email downloading

--- src/data/emails.py
+++ src/data/emails.py
@@ -1,18 +1,26 @@
-from ietfdata.mailarchive import MailArchive
-import pandas as pd
+import os
 import re
+
+import pandas as pd
+from ietfdata.mailarchive import MailArchive
 
 QUOTE_PATTERN = re.compile(rb'^>.*$', flags=re.MULTILINE)
 QUOTE_START_PATTERN = re.compile(rb'On .+ wrote:')
 WHITESPACE_PATTERN = re.compile(rb'\s+')
 
 
-def download_emails(mail_list: str, archive=None, output_file="data/raw/emails.pkl"):
+def download_emails(mail_list: str, archive=None, output_dir="data/raw"):
     if archive is None:
         archive = MailArchive()
 
+    print("Downloading emails...")
     m_list = archive.mailing_list(mail_list)
     messages = m_list.messages()
+
+    output_path = os.path.join(output_dir, mail_list)
+
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
 
     messages_dict = []
     for message in messages:
@@ -26,7 +34,7 @@ def download_emails(mail_list: str, archive=None, output_file="data/raw/emails.p
         })
 
     df = pd.DataFrame(messages_dict)
-    df.to_pickle(output_file)
+    df.to_pickle(os.path.join(output_path, "emails.pkl"))
 
 
 def clean_content(content):
