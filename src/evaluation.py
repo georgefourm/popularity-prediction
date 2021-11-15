233a9d6a9285a5d1b56b779c2fbaeff28f98c262
George Fourmouzis <georgefourm@gmail.com>
2021-11-15 11:30:56 +0200

    Fix cross-validation evaluation to account for continuous targets

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,17 +1,25 @@
 import pandas as pd
-from sklearn.model_selection import cross_validate, StratifiedKFold
+from sklearn.model_selection import cross_validate, StratifiedKFold, KFold
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.utils.multiclass import type_of_target
 
 
 def evaluate_model(model, metrics, data, targets):
     model = Pipeline([("std", StandardScaler()), ("estimator", model)])
+
+    # Explicitly configure for repeatable results
+    if type_of_target(targets) == "continuous":
+        cv = KFold(shuffle=True, random_state=1)
+    else:
+        cv = StratifiedKFold(shuffle=True, n_splits=5, random_state=1)
+
     scores = cross_validate(
         model,
         data,
         targets,
         scoring=metrics,
-        cv=StratifiedKFold(shuffle=True, n_splits=5, random_state=1)  # Explicitly configure for repeatable results
+        cv=cv
     )
     return scores
 
